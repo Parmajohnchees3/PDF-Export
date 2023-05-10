824d040aa6c3e23cfaa811c0564e55cfd367e28d
John Yoo <jsy12@duke.edu>
2023-05-10 15:58:00 -0500

    can draw & console shows rectangles

--- Backend/app.py
+++ Backend/app.py
@@ -1,8 +1,24 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import json
+
+
 app = Flask(__name__)
+cors = CORS(app, resources={r"/process_rectangles": {"origins": "http://localhost:3000"}})
 
 @app.route('/')
 def start():
     return "Hello John!"
 
-@app.route('/api/process_data/')
+@app.route('/process_rectangles', methods = ['POST'])
+def handle_rectangles():
+    data = request.get_json()
+    rectangles = data.get('rectangles')
+
+    print(f'Received rectangles: {rectangles}')  # Print rectangles to console for debugging
+
+    response_data = {'success' : True}
+    return jsonify(response_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- my-app/src/PDFUploader.js
+++ my-app/src/PDFUploader.js
@@ -1,108 +1,124 @@
 import React, { useRef, useState } from "react";
 import * as pdfjsLib from "pdfjs-dist";
 import "pdfjs-dist/build/pdf.worker.entry";
+import axios from 'axios';
 
 pdfjsLib.GlobalWorkerOptions.workerSrc = "pdf.worker.js";
 
 const PDFUploader = () => {
-  const fileRef = useRef();
-  const canvasRef = useRef();
-  const [isDrawing, setIsDrawing] = useState(false);
-  const [rectangles, setRectangles] = useState([]);
-  const [startPos, setStartPos] = useState({ x: 0, y: 0 });
-
-  const onFileChange = (event) => {
-    const file = event.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = async (e) => {
-        const data = new Uint8Array(e.target.result);
-        await onFileUpload(data);
-      };
-      reader.readAsArrayBuffer(file);
-    }
-  };
-
-  const onFileUpload = async (pdfData) => {
-    if (pdfData) {
-      const canvas = canvasRef.current;
-      const context = canvas.getContext("2d");
+    const fileRef = useRef();
+    const canvasRef = useRef();
+    const [isDrawing, setIsDrawing] = useState(false);
+    const [rectangles, setRectangles] = useState([]);
+    const [startPos, setStartPos] = useState({ x: 0, y: 0 });
+    const [endPos, setEndPos] = useState({ x: 0, y: 0 });
   
-      const pdf = await pdfjsLib.getDocument({ data: pdfData }).promise;
-      const page = await pdf.getPage(1);
-      const viewport = page.getViewport({ scale: 1 });
-  
-      canvas.height = viewport.height;
-      canvas.width = viewport.width;
-  
-      await page.render({ canvasContext: context, viewport }).promise;
-  
-      // Draw previous rectangles
-      rectangles.forEach((rect) => {
-        context.strokeRect(rect.x, rect.y, rect.width, rect.height);
-      });
-    }
-  };
-
-  const startDrawing = ({ nativeEvent }) => {
-    const { offsetX, offsetY } = nativeEvent;
-    setStartPos({ x: offsetX, y: offsetY });
-    setIsDrawing(true);
-  };
-
-  const draw = ({ nativeEvent }) => {
-    if (!isDrawing) return;
-    const { offsetX, offsetY } = nativeEvent;
-    const canvas = canvasRef.current;
-    const context = canvas.getContext("2d");
-  
-    const width = Math.abs(offsetX - startPos.x);
-    const height = Math.abs(offsetY - startPos.y);
-  
-    // Clear only the area where the current rectangle is being drawn
-    const x = Math.min(startPos.x, offsetX);
-    const y = Math.min(startPos.y, offsetY);
-    context.clearRect(x, y, width, height);
-  
-    // Draw previous rectangles
-    rectangles.forEach((rect) => {
-      context.strokeRect(rect.x, rect.y, rect.width, rect.height);
-    });
-  
-    // Draw the new rectangle
-    context.strokeRect(x, y, width, height);
-  };
-
-  const stopDrawing = ({ nativeEvent }) => {
-    setIsDrawing(false);
-    const { offsetX, offsetY } = nativeEvent;
-    const width = Math.abs(offsetX - startPos.x);
-    const height = Math.abs(offsetY - startPos.y);
-    const rect = {
-      x: Math.min(startPos.x, offsetX),
-      y: Math.min(startPos.y, offsetY),
-      width,
-      height,
+    const onFileChange = (event) => {
+      const file = event.target.files[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = async (e) => {
+          const data = new Uint8Array(e.target.result);
+          await onFileUpload(data);
+        };
+        reader.readAsArrayBuffer(file);
+      }
     };
-    setRectangles((prevRectangles) => [...prevRectangles, rect]);
-  };
+    
+    const onFileUpload = async (pdfData) => {
+      if (pdfData) {
+        const canvas = canvasRef.current;
+        const context = canvas.getContext("2d");
   
+        const pdf = await pdfjsLib.getDocument({ data: pdfData }).promise;
+        const page = await pdf.getPage(1);
+        const viewport = page.getViewport({ scale: 1 });
+  
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+  
+        await page.render({ canvasContext: context, viewport }).promise;
+      }
+    };
+  
+    const startDrawing = ({ nativeEvent }) => {
+      const { offsetX, offsetY } = nativeEvent;
+      setStartPos({ x: offsetX, y: offsetY });
+      setIsDrawing(true);
+    };
+  
+    const draw = ({ nativeEvent }) => {
+        if (!isDrawing) return;
+        const { offsetX, offsetY } = nativeEvent;
+        const canvas = canvasRef.current;
+        const context = canvas.getContext("2d");
+      
+        const width = Math.abs(offsetX - startPos.x);
+        const height = Math.abs(offsetY - startPos.y);
+      
+        // Clear only the area where the current rectangle is being drawn
+        const x = Math.min(startPos.x, offsetX);
+        const y = Math.min(startPos.y, offsetY);
+        context.clearRect(x, y, width, height);
+      
+        // Draw previous rectangles
+        rectangles.forEach((rect) => {
+          context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+        });
+      
+        // Draw the new rectangle
+        context.strokeRect(x, y, width, height);
+      };
+
+    const stopDrawing = ({ nativeEvent }) => {
+        setIsDrawing(false);
+        const { offsetX, offsetY } = nativeEvent;
+        setEndPos({ x: offsetX, y: offsetY });
+    
+        const width = Math.abs(offsetX - startPos.x);
+        const height = Math.abs(offsetY - startPos.y);
+        const rect = {
+        x: Math.min(startPos.x, offsetX),
+        y: Math.min(startPos.y, offsetY),
+        width,
+        height,
+        };
+        setRectangles((prevRectangles) => [...prevRectangles, rect]);
+    };
+      
+    const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+        const response = await axios.post('http://127.0.0.1:5000/process_rectangles', { rectangles });
+        if (response.status === 200) {
+        console.log('Rectangles processed successfully');
+        } else {
+        console.error('Failed to process rectangles');
+        }
+    } catch (error) {
+        console.error(error);
+        }
+    };
+
     return (
-      <div>
-        <h1>PDF Export</h1>
-        <form>
-          <input type="file" ref={fileRef} onChange={onFileChange} accept=".pdf" />
+        <div>
+            <h1>PDF Export</h1>
+            <form onSubmit={handleSubmit}>
+                <input type="file" ref={fileRef} onChange={onFileChange} accept=".pdf" />
+                {rectangles.length > 0 && (
+                    <button type="submit">Submit</button>
+        )}
         </form>
         <canvas
-          ref={canvasRef}
-          style={{ border: "1px solid black" }}
-          onMouseDown={startDrawing}
-          onMouseMove={draw}
-          onMouseUp={stopDrawing}
-          onMouseLeave={stopDrawing}
+            ref={canvasRef}
+            style={{ border: "1px solid black" }}
+            onMouseDown={startDrawing}
+            onMouseMove={draw}
+            onMouseUp={stopDrawing}
+            onMouseLeave={stopDrawing}
         ></canvas>
-      </div>
-    );
-};
+        </div>
+      );
+    };
   
 export default PDFUploader;
